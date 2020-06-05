11f3f4ce10a2d271709efef13a65a8c92ff1f7d4
Ashutosh-Nair17 <63490736+Ashutosh-Nair17@users.noreply.github.com>
2020-06-05 18:09:18 +0530

    Delete arays.py

--- arays.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# define list with values
-basicList = [[1], ["Town"], [4.6],["lol"]]
-
-# open output file for writing
-with open('listfile.txt', 'w') as filehandle:
-    json.dump(basicList, filehandle)
